713e9c710fb453520a5cdfe94b6a629a9939251b
unknown <i.ivanov_1987@abv.bg>
2018-10-02 14:58:46 +0300

    Adding 3rd part components

--- DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5000/api/auth/';
+baseUrl = 'http://localhost:5000/api/auth/';
+jwtHelper = new JwtHelperService();
+decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -18,6 +21,9 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        // get user from token in local storege
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );
@@ -26,4 +32,11 @@ login(model: any) {
  register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
  }
+
+ loggedIn() {
+   const token = localStorage.getItem('token');
+   // retun if is not Expired
+    return !this.jwtHelper.isTokenExpired(token);
+ }
+
 }
--- DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +11,29 @@ export class NavComponent implements OnInit {
 
   model: any = { };
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {
   }
 
   login() {
-    // console.log(this.model);
     this.authService.login(this.model).subscribe(next => {
-      console.log('login successfully');
+    this.alertify.success('success');
     }, error => {
-      console.log(error);
-     // console.log('login error');
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !! token;   // return true or false
+    return this.authService.loggedIn();
+
+    // const token = localStorage.getItem('token');
+    // return !! token;   // return true or false
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logout');
+    this.alertify.message('logout');
+    // console.log('logout');
   }
 }
